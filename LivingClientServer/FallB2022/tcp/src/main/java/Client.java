51474e61f1102c94464a23f830560b97d1c6986f
Ian Skelskey <ianskelskey@gmail.com>
2022-11-01 17:49:41 -0400

    Add TCP Message passing

--- LivingClientServer/FallB2022/tcp/src/main/java/Client.java
+++ LivingClientServer/FallB2022/tcp/src/main/java/Client.java
@@ -1,16 +1,30 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.Socket;
 
 class Client {
 
     public static void main(String[] args) {
         String host = args[0];
-
         int port = Integer.parseInt(args[1]);
 
-        try (Socket sock = new Socket(host, port)) {
-            System.out.println("Host: " + host + "\nPort: " + sock.getPort());
-        } catch (Exception e) {
+        try (Socket sock = new Socket(host, port);
+             BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
+             BufferedReader in = new BufferedReader(new InputStreamReader(sock.getInputStream()));
+             PrintWriter out = new PrintWriter(sock.getOutputStream()))
+        {
+            System.out.println("Host: " + host + "\nPort: " + port);
+            System.out.println("Enter a message for the server to echo!\n");
+            String userInput = stdIn.readLine();
+            System.out.println("You entered: " + userInput);
+            out.println(userInput);
+            out.flush();
+            System.out.println(in.readLine());
+        } catch(Exception e) {
             e.printStackTrace();
         }
+
     }
+
 }
--- LivingClientServer/FallB2022/tcp/src/main/java/Server.java
+++ LivingClientServer/FallB2022/tcp/src/main/java/Server.java
@@ -1,18 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 class Server {
-    public static void main (String[] args) {
-
+    public static void main(String[] args) {
         int port = Integer.parseInt(args[0]);
 
-        System.out.println("Server now listening on port: " + port);
-
-        try (ServerSocket serverSocket = new ServerSocket(port); Socket socket = serverSocket.accept()) {
+        try (ServerSocket serv = new ServerSocket(port);
+             Socket sock = serv.accept();
+             BufferedReader in = new BufferedReader(new InputStreamReader(sock.getInputStream()));
+             PrintWriter out = new PrintWriter(sock.getOutputStream(), true))
+        {
             System.out.println("New client has connected on port: " + port);
-            // Handle Clients/ Keep Listening
-        } catch (Exception e) {
-            e.printStackTrace();
+            String incoming = in.readLine();
+            System.out.println("Received: " + incoming);
+            out.println("Echo: " + incoming);
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
     }
 }
